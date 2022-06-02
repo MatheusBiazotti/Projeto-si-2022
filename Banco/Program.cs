aba1d38fd71eef44577a51004e1955b489258019
MATHEUS BIAZZOTI <2880972113037@labfatecaqa.com>
2022-06-02 20:04:19 -0300

    Projeto inicial

---
+++ Banco/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Banco.classes;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+            {
+                Cliente cli = new Cliente();
+                //dados do cliente matheus B //nome, Telefone, telefone, cpf, nascimento SEGUINDO A ORDEM do Cliente.cs
+                var ClientePrincipal = Cliente.CreateCliente("Matheus B", "Av doutor edson baccarin", 16997009743, "111.111.111-11", new DateTime(1999, 11, 20));
+                Console.WriteLine(ClientePrincipal.ToString());
+                //dados do cliente matheus R //seguindo a mesma ordem acima
+                var ClienteSecundario = Cliente.CreateCliente("Matheus R", "Av jose alberto", 1698059954, "222.222.222-22", new DateTime(2002, 05, 20));
+                Console.WriteLine(ClienteSecundario.ToString());
+
+                Conta ContaPoupancaOne = new Poupanca(ClientePrincipal, 12297, 2000);
+                Console.WriteLine($"Matheus B : {ContaPoupancaOne.Saldo}");
+
+                Conta ContaCorrenteTwo = new Corrente(ClienteSecundario, 12297, 300);
+                Console.WriteLine($"Matheus R : {ContaCorrenteTwo.Saldo}");
+                //Transferir dentro do ContaPoupancaOne
+                ContaPoupancaOne.Trasferir(1000, ContaCorrenteTwo);//valor a ser transferido
+
+                Console.WriteLine($"Matheus B = {ContaCorrenteTwo.Saldo}");
+                Console.WriteLine($"Matheus R = {ContaPoupancaOne.Saldo}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+               
+            }
+        }
+    }
+}
---
+++ Banco/classes/Poupanca.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.classes
+{
+    class Poupanca : Conta
+    {
+        public Poupanca(Cliente cliente, int numero, decimal saldo)
+           : base( cliente,  numero,  saldo)
+        {
+
+        }    
+        public override bool Depositar(decimal ValorDepositar)
+        {
+            if(ValorDepositar <= 0 )
+            {
+                Mensaguemtrans = $"O valor do deposito é invalido !";
+                return false;
+            }
+            Saldo += (ValorDepositar + 0.5m);
+            Mensaguemtrans = $"Depositado com sucesso";
+            return true;
+        }      
+
+        public override bool Sacar(decimal ValorSaque)
+        {
+            if(Saldo <= 0m)
+            {
+                Mensaguemtrans = $"Saldo da conta negativo ou zerado";
+                return false;
+            }
+            if(Saldo < ValorSaque)
+            {
+                Mensaguemtrans = $"Valor indisponivel para saque, valor na conta =R${Saldo}";
+                return false;
+            }
+            Saldo -= (ValorSaque - 0.10m);
+            Mensaguemtrans = "Saque realizado com sucesso";
+                return true;
+                
+                
+        }
+    }
+}
